3469f07b963b25166eea46d4ef9894e2eac0599c
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-08-02 23:52:49 +0200

    Fetch details of tombs asynchronously and return array when finished

--- src/APICalls.js
+++ src/APICalls.js
@@ -7,12 +7,14 @@ export const getListOfTombs = () =>
       data.query.categorymembers.map(entry => entry.title.split(' ').join('_'))
     );
 
-export const getDetailsOfTombs = array => {
+export const getDetailsOfTombs = async array => {
   let listOfTombObjects = [];
-  array.forEach(title =>
-    fetch(`https://de.wikipedia.org/api/rest_v1/page/summary/${title}`)
-      .then(results => results.json())
-      .then(data => listOfTombObjects.push(data))
+  await Promise.all(
+    array.map(title =>
+      fetch(`https://de.wikipedia.org/api/rest_v1/page/summary/${title}`).then(
+        results => results.json().then(data => listOfTombObjects.push(data))
+      )
+    )
   );
-  console.log(listOfTombObjects);
+  return listOfTombObjects;
 };
