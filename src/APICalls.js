1e7f3eac523235d4eef7bea90ed245d1a111bb0a
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-08-02 15:17:45 +0200

    Clean API request for tomb details, still returning weird array

--- src/APICalls.js
+++ src/APICalls.js
@@ -3,4 +3,16 @@ export const getListOfTombs = () =>
     'https://de.wikipedia.org/w/api.php?action=query&cmlimit=100&list=categorymembers&cmpageid=9640102&origin=*&format=json'
   )
     .then(res => res.json())
-    .then(data => data.query.categorymembers);
+    .then(data =>
+      data.query.categorymembers.map(entry => entry.title.split(' ').join('_'))
+    );
+
+export const getDetailsOfTombs = array => {
+  let listOfTombObjects = [];
+  array.forEach(title =>
+    fetch(`https://de.wikipedia.org/api/rest_v1/page/summary/${title}`)
+      .then(results => results.json())
+      .then(data => listOfTombObjects.push(data))
+  );
+  console.log(listOfTombObjects);
+};
--- src/App.js
+++ src/App.js
@@ -9,19 +9,7 @@ class App extends Component {
   };
   async componentDidMount() {
     const tombs = await APICalls.getListOfTombs();
-    this.setState({tombs});
-    console.log(this.state.tombs);
-    // // Make an API request for each title in the list to retrieve detailed information
-    // listOfEntryTitles.forEach(title => {
-    //   fetch(`https://de.wikipedia.org/api/rest_v1/page/summary/${title}`)
-    //     .then(results => {
-    //       return results.json();
-    //     })
-    //     .then(data => {
-    //       listOfTombObjects.push(data);
-    //       console.log(listOfTombObjects);
-    //     });
-    // });
+    APICalls.getDetailsOfTombs(tombs);
   }
 
   render() {
