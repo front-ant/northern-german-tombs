d5cf5b02386a1614127abea1336f05118cb135b0
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-08-04 13:45:40 +0200

    Display a bunch of Markers

--- src/MapOfTombs.js
+++ src/MapOfTombs.js
@@ -8,12 +8,16 @@ import {
 
 const MapOfTombs = withScriptjs(
   withGoogleMap(props => {
-    console.log(props.coordinates);
-    let markers = <Marker position={{lat: 53.1572121, lng: 10.2079295}} />;
+    console.log(props.coordinates[0]);
+    // let markers = props.coordinates.forEach(coord => (
+    //   <Marker position={coord} />
+    // ));
 
     return (
       <GoogleMap defaultZoom={12} center={{lat: 53.1572121, lng: 10.2079295}}>
-        {markers}
+        {props.coordinates.map(coord => (
+          <Marker position={coord} key={coord.lat} />
+        ))}
       </GoogleMap>
     );
   })
