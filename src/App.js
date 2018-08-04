a4a926649d857e0ecc28f99f513d9e3e185e24ac
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-08-04 13:05:46 +0200

    Try using a MapContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import MapOfTombs from './MapOfTombs';
 import * as APICalls from './APICalls';
+import MapContainer from './MapContainer';
 
 class App extends Component {
   state = {
@@ -12,26 +12,18 @@ class App extends Component {
     const listOfTombs = await APICalls.getListOfTombs();
     const tombs = await APICalls.getDetailsOfTombs(listOfTombs);
     const filteredTombs = tombs.filter(tomb => tomb.coordinates);
-    this.setState({tombs: filteredTombs});
     const coordinates = filteredTombs.map(t => t.coordinates).map(tomb => {
       tomb.lng = tomb.lon;
       delete tomb.lon;
       return tomb;
     });
     this.setState({coordinates});
-    console.log(this.state.coordinates);
   }
 
   render() {
     return (
       <div className="App">
-        <MapOfTombs
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBvTq7xtoNgz7I1mMtLJ6US0PJ14KSZ4zg`}
-          loadingElement={<div style={{height: `100%`}} />}
-          containerElement={<div style={{height: `600px`, width: `100%`}} />}
-          mapElement={<div style={{height: `100%`}} />}
-          tombs={this.state.tombs}
-        />
+        <MapContainer />
       </div>
     );
   }
---
+++ src/MapContainer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import MapOfTombs from './MapOfTombs';
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <div className="Map">
+        <MapOfTombs
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBvTq7xtoNgz7I1mMtLJ6US0PJ14KSZ4zg`}
+          loadingElement={<div style={{height: `100%`}} />}
+          containerElement={<div style={{height: `600px`, width: `100%`}} />}
+          mapElement={<div style={{height: `100%`}} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default MapContainer;
